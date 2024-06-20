ad251eb1a47e1152ce11e4387b913eb94655c011
Matias Batista <Matias.Batista@dclsoft.com.br>
2024-06-19 23:05:32 -0300

    AJUSTE ROTAS PT.2

--- src/hangmo/App.js
+++ src/hangmo/App.js
@@ -1,58 +1,31 @@
 //to run = npm start
-
-import { AuthProvider } from "./contexts/AuthContext";
 import React from "react";
-import { Link } from "react-router-dom";
 import "./App.css";
 import "./login";
 import "./cadastro";
+import './index.css';
+import Login from './login';
+import Cadastro from './cadastro';
+import Hangmo from './hangmo';
+import Conta from './conta';
+import Ranking from './ranking';
+import Esqueci from './esqueci';
+import PrivateRoute from './privateRoute'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="App">
-        <header>
-          <h1 id="title">
-            <span style={{ color: "black" }}>HANG</span>
-            <span style={{ color: "white" }}>MO</span>
-          </h1>
-          <div class="buttons">
-            <Link to="/login">
-              <button>LOGIN</button>
-            </Link>
-            <Link to="/cadastro">
-              <button>CADASTRO</button>
-            </Link>
-          </div>
-        </header>
-        <div id="container">
-          <div id="black-square" />
-          <p id="initial-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Pellentesque orci elit, lobortis dignissim tincidunt volutpat,
-            aliquam at arcu. Quisque finibus nisl at orci volutpat condimentum.
-            Phasellus ut bibendum lacus. Vivamus hendrerit sodales ante, sit
-            amet vestibulum velit sodales a. Nullam tincidunt arcu nunc, a
-            suscipit mi facilisis sit amet. Nulla facilisi. Nullam a accumsan
-            tellus. Praesent efficitur elit quis velit pharetra tincidunt. Cras
-            tempor eleifend libero ut rutrum. Ut vel lacus in purus consequat
-            auctor. Maecenas in metus iaculis, dictum erat eu, blandit quam.
-            Morbi egestas neque in neque mollis, dignissim tincidunt justo
-            tempus. Aenean bibendum lorem lectus, vitae euismod tellus convallis
-            vel. Nullam rutrum sapien tellus, vitae suscipit felis volutpat nec.
-            Nunc eu efficitur est, a bibendum ex. Sed iaculis erat eu lorem
-            consequat feugiat.
-          </p>
-        </div>
-        <footer>
-          <p>
-            A3 do primeiro semestre de 2024 para as disciplinas de Sistemas
-            distribuídos e mobile e de Usabilidade, desenvolvimento web, mobile
-            e jogos - Universidade São Judas Tadeu
-          </p>
-        </footer>
-      </div>
-    </AuthProvider>
+    <Router>
+      <Switch>
+          <Route path="/login" element={<Login />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/esqueci" element={<Esqueci />} />
+          <PrivateRoute path="/hangmogame" element={<Hangmo />} />
+          <PrivateRoute path="/conta" element={<Conta />} />
+          <PrivateRoute path="/ranking" element={<Ranking />} />
+          <Route path="*" element={<Login />} />
+      </Switch>
+    </Router>
   );
 }
 export default App;
